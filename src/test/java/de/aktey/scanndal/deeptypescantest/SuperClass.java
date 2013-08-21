fff9fb2d03ecf84a59a489b08dad6efc7681da25
Ruben Wagner <ruben.wagner.de@googlemail.com>
2013-08-21 15:03:19 +0200

    add deep type scan

---
+++ src/test/java/de/aktey/scanndal/deeptypescantest/SuperClass.java
@@ -0,0 +1,10 @@
+package de.aktey.scanndal.deeptypescantest;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ruben
+ * Date: 04.08.13
+ * Time: 12:08
+ */
+public class SuperClass extends SuperSuperClass {
+}
