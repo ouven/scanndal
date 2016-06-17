f4bfd45d3ef36b36782b3c6e5ea6222f01992fb8
Nicole Spekat <nicole.spekat@holidaycheck.com>
2016-06-17 13:59:10 +0200

    new class file structures (java 1.8)

--- src/test/java/de/aktey/scanndal/classmappertest/ClassMapperTestClass.java
+++ src/test/java/de/aktey/scanndal/classmappertest/ClassMapperTestClass.java
@@ -1,5 +1,8 @@
 package de.aktey.scanndal.classmappertest;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created with IntelliJ IDEA.
  * User: ruben
@@ -9,5 +12,11 @@ package de.aktey.scanndal.classmappertest;
 public class ClassMapperTestClass {
     public String foo() {
         return "bar";
+    }
+    public void forEachLoop() {
+        List<String> list = Arrays.asList("a", "b");
+        list.forEach( (l) -> {
+            System.out.println(l);
+        });
     }
 }
